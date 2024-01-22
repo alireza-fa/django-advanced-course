3e1cf7165558333851ac6563f90827aa4a50a1bf
alireza-fa <alirezafeyze44@gmail.com>
2024-01-22 17:17:39 +0330

    add log_level const

---
+++ apps/utils/logging/category.py
@@ -0,0 +1,12 @@
+GENERAL = "general"
+Postgres = "postgres"
+Redis = "redis"
+Validation = "validation"
+RequestResponse = 'requestResponse'
+
+Select = "select"
+Rollback = "rollback"
+Update = "update"
+Rollback = "rollback"
+
+API = "api"
