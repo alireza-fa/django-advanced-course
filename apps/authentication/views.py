de93e8dbb7c5c95bf8535ccccb2c464550a57e81
alireza-fa <alirezafeyze44@gmail.com>
2024-01-24 06:33:12 +0330

    update base response

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,6 +1,4 @@
-from django.utils.translation import gettext_lazy as _
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
 
